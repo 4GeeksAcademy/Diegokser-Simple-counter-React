0de4d77c265dd8b923b5fd94ebba229de3e61029
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-05-15 19:17:53 +0000

    cambios en ficheros

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,21 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let value = 0;
+
+    setInterval(() => {
+        const six = Math.floor(value/100000);
+        const five = Math.floor(value/10000);
+        const four = Math.floor(value/1000);
+        const three = Math.floor(value/100);
+        const two = Math.floor(value/10);
+        const one = Math.floor(value/1)
+        value ++;
+        
+ReactDOM.render(
+            <Home  digitOne={one%10} digitTwo={two%10} digitThree={three%10} digitFour={four%10} digitFive={five%10} digitSix={six%10}/>,
+            document.querySelector('#app')
+           );   
+         }, 1000)
+
 
